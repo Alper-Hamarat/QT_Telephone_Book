11b52ce11a01e0ac17fa930796304a24de029439
Alper Hamarat <alper.hamarat@gmx.de>
2026-05-12 15:43:21 +0200

    Added Comments to Entry.h and .cpp

--- Source/QT_Telephone_Book/Entry.cpp
+++ Source/QT_Telephone_Book/Entry.cpp
@@ -1,14 +1,32 @@
 #include "Entry.h"
 
-Entry::Entry(string name, string telNr, string address) : name(name), telNr(telNr), address(address){}
+/*
+ * Creates a new entry with custom values.
+ */
+Entry::Entry(string name, string telNr, string address)
+    : name(name), telNr(telNr), address(address)
+{
+}
 
-Entry::Entry() : Entry("Max Mustermann","012345678", "Musterstadt"){};
+/*
+ * Creates a default entry with predefined values.
+ */
+Entry::Entry()
+    : Entry("Max Mustermann", "012345678", "Musterstadt")
+{
+}
 
+/*
+ * Returns the name of the entry.
+ */
 string Entry::getName()
 {
     return this->name;
 }
 
+/*
+ * Sets a new name if the value is not empty.
+ */
 void Entry::setName(string newName)
 {
     if(newName != "")
@@ -17,15 +35,22 @@ void Entry::setName(string newName)
     }
     else
     {
-        cout <<  "Name: " << newName <<  " has not been assigned to entry" << endl;
+        cout << "Name: " << newName
+             << " has not been assigned to entry" << endl;
     }
 }
 
+/*
+ * Returns the telephone number.
+ */
 string Entry::getTelNr()
 {
     return this->telNr;
 }
 
+/*
+ * Sets a new telephone number if the value is not empty.
+ */
 void Entry::setTelNr(string newTelNr)
 {
     if(newTelNr != "")
@@ -34,15 +59,22 @@ void Entry::setTelNr(string newTelNr)
     }
     else
     {
-        cout <<  "TelNr: " << newTelNr <<  " has not been assigned to entry" << endl;
+        cout << "TelNr: " << newTelNr
+             << " has not been assigned to entry" << endl;
     }
 }
 
+/*
+ * Returns the address.
+ */
 string Entry::getAddress()
 {
     return this->address;
 }
 
+/*
+ * Sets a new address if the value is not empty.
+ */
 void Entry::setAddress(string address)
 {
     if(address != "")
@@ -51,10 +83,14 @@ void Entry::setAddress(string address)
     }
     else
     {
-        cout <<  "Address: " << address <<  " has not been assigned to entry" << endl;
+        cout << "Address: " << address
+             << " has not been assigned to entry" << endl;
     }
 }
 
+/*
+ * Prints all entry information to the console.
+ */
 void Entry::printEntry()
 {
     cout << "-------------------------------" << endl;
--- Source/QT_Telephone_Book/Entry.h
+++ Source/QT_Telephone_Book/Entry.h
@@ -5,25 +5,68 @@
 
 using namespace std;
 
+/*
+ * Represents a single telephone book entry.
+ * Stores basic contact information such as
+ * name, telephone number and address.
+ */
 class Entry
 {
 protected:
+    // Name of the entry/person
     string name;
+
+    // Telephone number of the entry
     string telNr;
+
+    // Address of the entry
     string address;
+
 public:
+
+    /*
+     * Creates an entry with initialized values.
+     */
     Entry(string name, string telNr, string address);
+
+    /*
+     * Default constructor.
+     */
     Entry();
-    
+
+    /*
+     * Returns the name of the entry.
+     */
     virtual string getName();
+
+    /*
+     * Sets a new name for the entry.
+     */
     void setName(string newName);
 
+    /*
+     * Returns the telephone number.
+     */
     string getTelNr();
+
+    /*
+     * Sets a new telephone number.
+     */
     void setTelNr(string newTelNr);
 
+    /*
+     * Returns the address.
+     */
     string getAddress();
+
+    /*
+     * Sets a new address.
+     */
     void setAddress(string newAddress);
 
+    /*
+     * Prints the entry information.
+     */
     virtual void printEntry();
 };
 
