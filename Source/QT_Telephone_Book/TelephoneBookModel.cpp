27ae93279bf398ec4e759d2760951cfe0d601737
Alper Hamarat <alper.hamarat@gmx.de>
2026-05-12 15:50:17 +0200

    Added comments to model header and source

--- Source/QT_Telephone_Book/TelephoneBookModel.cpp
+++ Source/QT_Telephone_Book/TelephoneBookModel.cpp
@@ -2,11 +2,20 @@
 
 #include <optional>
 
+/*
+ * Returns all stored telephone book entries.
+ */
 vector<Entry> TelephoneBookModel::getEntries()
 {
     return listEntries;
 }
 
+/*
+ * Searches for an entry by name.
+ *
+ * Returns a pointer to the matching entry
+ * or nullptr if no entry was found.
+ */
 Entry* TelephoneBookModel::getEntry(std::string name)
 {
     for (Entry& entry : this->listEntries)
@@ -20,24 +29,41 @@ Entry* TelephoneBookModel::getEntry(std::string name)
     return nullptr;
 }
 
+/*
+ * Adds a new entry to the model.
+ *
+ * Returns false if an entry with
+ * the same name already exists.
+ */
 bool TelephoneBookModel::addEntry(Entry& newEntry)
 {
+    // Prevent duplicate entries
     if (getEntry(newEntry.getName()) != nullptr)
     {
         return false;
     }
 
+    // Store the new entry
     listEntries.push_back(newEntry);
+
     return true;
 }
 
+/*
+ * Removes an entry identified by name.
+ *
+ * Returns true if the entry was removed.
+ */
 bool TelephoneBookModel::removeEntry(std::string name)
 {
-    for (auto it = listEntries.begin(); it != listEntries.end(); ++it)
+    for (auto it = listEntries.begin();
+         it != listEntries.end();
+         ++it)
     {
         if (it->getName() == name)
         {
             listEntries.erase(it);
+
             return true;
         }
     }
@@ -45,9 +71,17 @@ bool TelephoneBookModel::removeEntry(std::string name)
     return false;
 }
 
+/*
+ * Updates an existing entry
+ * with new data.
+ *
+ * Returns false if the entry
+ * does not exist.
+ */
 bool TelephoneBookModel::editEntry(std::string name,
                                    Entry& newEntry)
 {
+    // Search for the existing entry
     Entry* oldEntry = getEntry(name);
 
     if (oldEntry == nullptr)
@@ -55,6 +89,8 @@ bool TelephoneBookModel::editEntry(std::string name,
         return false;
     }
 
+    // Replace old entry data
     *oldEntry = newEntry;
+
     return true;
 }
--- Source/QT_Telephone_Book/TelephoneBookModel.h
+++ Source/QT_Telephone_Book/TelephoneBookModel.h
@@ -6,18 +6,58 @@
 
 #include "Entry.h"
 
+/*
+ * Stores and manages all telephone
+ * book entries.
+ *
+ * Provides CRUD operations for
+ * accessing and modifying entries.
+ */
 class TelephoneBookModel
 {
 private:
+
+    // Internal container holding all entries
     std::vector<Entry> listEntries;
 
 public:
 
+    /*
+     * Returns all stored entries.
+     */
     std::vector<Entry> getEntries();
+
+    /*
+     * Returns a pointer to an entry
+     * identified by its name.
+     *
+     * Returns nullptr if no entry exists.
+     */
     Entry* getEntry(std::string name);
+
+    /*
+     * Adds a new entry to the model.
+     *
+     * Returns true on success.
+     */
     bool addEntry(Entry& newEntry);
+
+    /*
+     * Removes an entry identified
+     * by its name.
+     *
+     * Returns true on success.
+     */
     bool removeEntry(std::string name);
-    bool editEntry(std::string name, Entry& newEntry);
+
+    /*
+     * Updates an existing entry
+     * with new values.
+     *
+     * Returns true on success.
+     */
+    bool editEntry(std::string name,
+                   Entry& newEntry);
 };
 
 #endif /* TELEPHONEBOOKMODEL_H */
